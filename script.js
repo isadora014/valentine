fc02d453c20d98d7a3ce9123b8672246106ee1de
isadora014 <ashleydossousisa@gmail.com>
2026-02-13 17:36:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,25 @@ function randomLove() {
     text.innerText = messages[randomIndex];
     text.style.display = "block";
 }
+function showSurprise() {
+    const text = document.getElementById("surpriseText");
+    text.style.display = "block";
+}
+
+const heartsContainer = document.querySelector(".hearts");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = 5 + Math.random() * 5 + "s";
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 8000);
+}
+
+setInterval(createHeart, 500);
+
 
